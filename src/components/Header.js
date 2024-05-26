affb38afd3c262d13d155679037c1b4e30d2608e
Taha Elsayed <tahazayed@gmail.com>
2024-05-26 19:11:06 -0400

    Update Header.js
    change home link

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ function Header() {
       <div className="profile-page sidebar-collapse">
         <nav className="navbar navbar-expand-lg fixed-top navbar-transparent bg-primary" color-on-scroll="400">
           <div className="container">
-            <div className="navbar-translate"><a className="navbar-brand" href="#" rel="tooltip">Home</a>
+            <div className="navbar-translate"><a className="navbar-brand" href="/" rel="tooltip">Home</a>
               <button className="navbar-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-bar bar1"></span><span className="navbar-toggler-bar bar2"></span><span className="navbar-toggler-bar bar3"></span></button>
             </div>
             <div className="collapse navbar-collapse justify-content-end" id="navigation">
